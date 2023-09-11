81f7434872849aad36c8f03038e3ca9626140adc
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-09-11 17:03:09 +0200

    Add component for cards

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template lang="">
-  <div class="container">
-      <h1>ciao</h1>
-  </div>
-</template>
-<script>
-export default {
+<!-- header -->
+<AppHeader />
+<!-- cards -->
+<AppCard :cardContents="projects"/>
 
-}
+</template>
+
+<script>
+const endpoint = "http://127.0.0.1:8000/api/projects";
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+import AppCard from "./components/AppCard.vue";
+
+export default {
+  components: { AppHeader, AppCard },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then((res) => {
+        this.projects = res.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+
+<style lang=""></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <div class="container my-3">
+    <div class="card mb-3" v-for="cardContent in cardContents">
+      <div class="row g-0">
+        <div class="col-md-4" v-if="cardContent.image">
+          <img src="" class="img-fluid rounded-start" alt="" />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{ cardContent.title }}</h5>
+            <p class="card-text">
+              {{ cardContent.content }}
+            </p>
+            <p class="card-text">
+              <small class="text-muted">{{ cardContent.created_at }}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: { cardContents: Array }
+};
+
+</script>
+<style lang="">
+</style>
